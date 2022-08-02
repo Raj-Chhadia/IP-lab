4877ad9917192be9cfee500d8202d505885f21df
Raj-Chhadia <raj.chhadia@gmail.com>
2022-08-02 13:00:51 +0530

    new changes

---
+++ Prg/IF.m
@@ -0,0 +1,10 @@
+% 92000103073 - Raj Chhadia
+a = 100;
+b = 200;
+   if( a == 100 )
+      if( b == 200 )
+         fprintf('Value of a is 100 and b is 200\n' );
+      end       
+   end
+   fprintf('Exact value of a is : %d\n', a );
+   fprintf('Exact value of b is : %d\n', b );
---
+++ Prg/Untitled2.m
@@ -0,0 +1,16 @@
+% 92000103073 - Raj Chhadia
+grade = 'B';
+   switch(grade)
+   case 'A' 
+      fprintf('Excellent!\n' );
+   case 'B' 
+      fprintf('Well done\n' );
+   case 'C' 
+      fprintf('Well done\n' );
+   case 'D'
+      fprintf('You passed\n' );
+   case 'F' 
+      fprintf('Better try again\n' );
+   otherwise
+      fprintf('Invalid grade\n' );
+   end
