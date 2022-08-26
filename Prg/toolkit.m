e81ef57bd283ca00eb8de4f5316115f7ccdd066f
Raj-Chhadia <raj.chhadia@gmail.com>
2022-08-26 21:58:28 +0530

    new programs added

--- Prg/toolkit.m
+++ Prg/toolkit.m
@@ -1,4 +1,4 @@
 % 92000103073 - Raj Chhadia
-I = imread('rice.png');
-J = imresize(I, 0.5);
-figure, imshow(I), figure, imshow(J)
+x=imread('pepper.jpg');
+size(x,1)
+size(x,2)
---
+++ Prg/uiget.m
@@ -0,0 +1,22 @@
+[file,path]=uigetfile('*.jpg');
+r=imread(path);
+s=r;
+
+subplot(1,3,1);
+    imshow (r);
+    title('original image');
+
+subplot(1,3,2);
+    for row = 1 : size(r,1)
+        for col = 1 : size(r,2)
+            s(row,col) = 255 - r(row,col);
+        end
+    end
+    imshow (s);
+    title('negative image');
+
+subplot(1,3,3);
+    q=imcomplement(r);
+    imshow(q);
+    title('using imcomplement()');
+fprintf('92000103073 Raj Chhadia');
